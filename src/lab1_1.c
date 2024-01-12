0cdbfc441644424615668196475bcd982957201c
Katehok <a.timin2003@mail.ru>
2024-01-12 23:33:02 +0300

    Done reports, modifyed table

--- src/lab1_1.c
+++ src/lab1_1.c
@@ -1,36 +1,42 @@
+﻿#define _CRT_RAND_S
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
+
+void qwerty(const int threads, const int* array, const unsigned int count)
+{
+    double start, end;
+    int max = -1;
+    int i;
+    start = omp_get_wtime();
+    #pragma omp parallel num_threads(threads) reduction(max: max) private(i)
+    {
+        #pragma omp for
+        for (i = 0; i < count; ++i) if (array[i] > max) max = array[i];
+        //printf("%2d: My lmax is: %d;\n", omp_get_thread_num(), max);
+
+    }
+    end = omp_get_wtime();
+    printf("Max is: %d\nTime: %f\n======\n", max, end - start);
+}
 
 int main(int argc, char** argv)
 {
-    const int count = 10000000;     ///< Number of array elements
-    const int threads = 16;         ///< Number of parallel threads to use
-    const int random_seed = 920215; ///< RNG seed
+    const unsigned int c = 100000000;
 
-    int* array = 0;                 ///< The array we need to find the max in
-    int  max   = -1;                ///< The maximal element
+    int* arr = (int*)malloc(10 * c * sizeof(int));
+    if (arr == NULL) return 1;
 
-    /* Initialize the RNG */
-    srand(random_seed);
+    printf("OpenMP: %d\n", _OPENMP);
+    for (int i = 0; i < 10 * c; i++) rand_s(arr + i);
 
-    /* Determine the OpenMP support */
-    //printf("OpenMP: %d;\n======\n", _OPENMP);
-
-    /* Generate the random array */
-    array = (int*)malloc(count*sizeof(int));
-    for(int i=0; i<count; i++) { array[i] = rand(); }
-
-    /* Find the maximal element */
-    /*#pragma omp parallel num_threads(threads) shared(array, count) reduction(max: max) default(none)
-    {*/
-        //#pragma omp for
-        for(int i=0; i<count; i++)
-        {
-            if(array[i] > max) { max = array[i]; };
+    for (int th = 1; th <= 16; ++th)
+        for (int i = 0; i < 10; ++i) { 
+            printf("\n======\nThreads: %2d; Array: %d\n", th, i);
+            qwerty(th, arr + i * c, c); 
         }
-        printf("-- My lmax is: %d;\n", max);
-    //}
 
-    printf("======\nMax is: %d;\n", max);
-    return(0);
+    free(arr);
+
+	return 0;
 }
